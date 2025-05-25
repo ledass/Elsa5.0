03e9f18333de410ba39e120a4a315f0e518bf19d
ledass <nistalieocs@gmail.com>
2025-05-25 09:06:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -49,10 +49,6 @@ async def start(client, message):
         ]]
         
         reply_markup = InlineKeyboardMarkup(buttons)
-        #add emoji loading then run 1 sec and dlt
-        m=await message.reply_text("ShobanaFilterBot") 
-        await asyncio.sleep(1.2)#1.2sec sleep
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
